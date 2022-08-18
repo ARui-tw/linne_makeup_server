67b942aa45d19a85ba9ac1de4de0043376a8c3d9
35538785 <p21y437@gmail.com>
2022-08-19 07:55:33 +0800

    feat: add authentication into routes

--- routes/keyword.js
+++ routes/keyword.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import controller from '../controller';
+import authentication from '../middleware/authentication';
 
 const keywordRouter = express.Router();
 
-keywordRouter.post('/createKeyword', controller.keyword.createKeyword);
-keywordRouter.post('/modifyKeyword', controller.keyword.modifyKeyword);
-keywordRouter.post('/getKeyword', controller.keyword.getKeyword);
-keywordRouter.post('/getKeywords', controller.keyword.getKeywords);
-keywordRouter.post('/removeKeyword', controller.keyword.removeKeyword);
+keywordRouter.post('/createKeyword', authentication(), controller.keyword.createKeyword);
+keywordRouter.post('/modifyKeyword', authentication('admin'), controller.keyword.modifyKeyword);
+keywordRouter.post('/getKeyword', authentication(), controller.keyword.getKeyword);
+keywordRouter.post('/getKeywords', authentication(), controller.keyword.getKeywords);
+keywordRouter.post('/removeKeyword', authentication('admin'), controller.keyword.removeKeyword);
 
 export default keywordRouter;
