f258f61dd963129842e8125bc80f29fe4c5dc966
35538785 <p21y437@gmail.com>
2022-08-19 07:59:03 +0800

    feat: add authentication into routes

--- routes/photoRelative.js
+++ routes/photoRelative.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import controller from '../controller';
+import authentication from '../middleware/authentication';
 
 const photoRelativeRouter = express.Router();
 
-photoRelativeRouter.post('/createPhotoRelative', controller.photoRelative.createPhotoRelative);
-photoRelativeRouter.post('/modifyPhotoRelative', controller.photoRelative.modifyPhotoRelative);
-photoRelativeRouter.post('/getPhotoRelative', controller.photoRelative.getPhotoRelative);
-photoRelativeRouter.post('/getPhotoRelatives', controller.photoRelative.getPhotoRelatives);
-photoRelativeRouter.post('/removePhotoRelative', controller.photoRelative.removePhotoRelative);
+photoRelativeRouter.post('/createPhotoRelative', authentication(), controller.photoRelative.createPhotoRelative);
+photoRelativeRouter.post('/modifyPhotoRelative', authentication('admin'), controller.photoRelative.modifyPhotoRelative);
+photoRelativeRouter.post('/getPhotoRelative', authentication('admin'), controller.photoRelative.getPhotoRelative);
+photoRelativeRouter.post('/getPhotoRelatives', authentication('admin'), controller.photoRelative.getPhotoRelatives);
+photoRelativeRouter.post('/removePhotoRelative', authentication('admin'), controller.photoRelative.removePhotoRelative);
 
 export default photoRelativeRouter;
