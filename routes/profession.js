bce2e0e17deedc15bb740b9216262329ef60c6d6
ARui <chen108060030@gapp.nthu.edu.tw>
2022-09-05 09:43:15 +0800

    feat: add authentication into routes

--- routes/profession.js
+++ routes/profession.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import controller from '../controller';
+import authentication from '../middleware/authentication';
 
 const professionRouter = express.Router();
 
 professionRouter.post('/createProfession', controller.profession.createProfession);
-professionRouter.post('/modifyProfession', controller.profession.modifyProfession);
-professionRouter.post('/getProfession', controller.profession.getProfession);
-professionRouter.post('/getProfessions', controller.profession.getProfessions);
-professionRouter.post('/removeProfession', controller.profession.removeProfession);
+professionRouter.post('/modifyProfession', authentication('admin'), controller.profession.modifyProfession);
+professionRouter.post('/getProfession', authentication('admin'), controller.profession.getProfession);
+professionRouter.post('/getProfessions', authentication('admin'), controller.profession.getProfessions);
+professionRouter.post('/removeProfession', authentication('admin'), controller.profession.removeProfession);
 
 export default professionRouter;
