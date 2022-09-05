ae4ab45e6b95b2e3fcc0bd7ff251e38c4721b52f
ARui <chen108060030@gapp.nthu.edu.tw>
2022-09-05 10:20:10 +0800

    feat: add timestamps for AS, RS and User

--- models/RelativeScore.js
+++ models/RelativeScore.js
@@ -1,10 +1,15 @@
 import mongoose from 'mongoose';
 
-const RelativeScoreSchema = new mongoose.Schema({
-  user_id: {
-    type: String,
-    required: true,
+const RelativeScoreSchema = new mongoose.Schema(
+  {
+    user_id: {
+      type: String,
+      required: true,
+    },
   },
-});
+  {
+    timestamps: true,
+  },
+);
 
-export default mongoose.model('Profession', RelativeScoreSchema);
+export default mongoose.model('RelativeScore', RelativeScoreSchema);
