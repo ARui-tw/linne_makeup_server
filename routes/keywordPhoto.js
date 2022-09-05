6e3937870363b75892bdc5a7f98771bc8cafe16d
ARui <chen108060030@gapp.nthu.edu.tw>
2022-09-05 16:30:21 +0800

    feat: add auth for KP

--- routes/keywordPhoto.js
+++ routes/keywordPhoto.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import controller from '../controller';
+import authentication from '../middleware/authentication';
 
 const keywordPhotoRouter = express.Router();
 
-keywordPhotoRouter.post('/createKeywordPhoto', controller.keywordPhoto.createKeywordPhoto);
-keywordPhotoRouter.post('/modifyKeywordPhoto', controller.keywordPhoto.modifyKeywordPhoto);
-keywordPhotoRouter.post('/getKeywordPhoto', controller.keywordPhoto.getKeywordPhoto);
-keywordPhotoRouter.post('/getKeywordPhotos', controller.keywordPhoto.getKeywordPhotos);
-keywordPhotoRouter.post('/removeKeywordPhoto', controller.keywordPhoto.removeKeywordPhoto);
+keywordPhotoRouter.post('/createKeywordPhoto', authentication('admin'), controller.keywordPhoto.createKeywordPhoto);
+keywordPhotoRouter.post('/modifyKeywordPhoto', authentication('admin'), controller.keywordPhoto.modifyKeywordPhoto);
+keywordPhotoRouter.post('/getKeywordPhoto', authentication('admin'), controller.keywordPhoto.getKeywordPhoto);
+keywordPhotoRouter.post('/getKeywordPhotos', authentication('admin'), controller.keywordPhoto.getKeywordPhotos);
+keywordPhotoRouter.post('/removeKeywordPhoto', authentication('admin'), controller.keywordPhoto.removeKeywordPhoto);
 
 export default keywordPhotoRouter;
