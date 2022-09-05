f2eed686936f26127b025111660a789db0290b81
ARui <chen108060030@gapp.nthu.edu.tw>
2022-09-05 10:24:16 +0800

    feat: add authentication into RS routes

--- routes/relativeScore.js
+++ routes/relativeScore.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import controller from '../controller';
+import authentication from '../middleware/authentication';
 
 const relativeScoreRouter = express.Router();
 
-relativeScoreRouter.post('/createRelativeScore', controller.relativeScore.createRelativeScore);
-relativeScoreRouter.post('/modifyRelativeScore', controller.relativeScore.modifyRelativeScore);
-relativeScoreRouter.post('/getRelativeScore', controller.relativeScore.getRelativeScore);
-relativeScoreRouter.post('/getRelativeScores', controller.relativeScore.getRelativeScores);
-relativeScoreRouter.post('/removeRelativeScore', controller.relativeScore.removeRelativeScore);
+relativeScoreRouter.post('/createRelativeScore', authentication(), controller.relativeScore.createRelativeScore);
+relativeScoreRouter.post('/modifyRelativeScore', authentication('admin'), controller.relativeScore.modifyRelativeScore);
+relativeScoreRouter.post('/getRelativeScore', authentication('admin'), controller.relativeScore.getRelativeScore);
+relativeScoreRouter.post('/getRelativeScores', authentication('admin'), controller.relativeScore.getRelativeScores);
+relativeScoreRouter.post('/removeRelativeScore', authentication('admin'), controller.relativeScore.removeRelativeScore);
 
 export default relativeScoreRouter;
